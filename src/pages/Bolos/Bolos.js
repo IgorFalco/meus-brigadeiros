6f49ad0ecf22070dcd1e7d9e1e4787ca89b2e1a3
Igor Falco <igorfalco@cpe.com.br>
2022-05-02 19:51:03 -0300

    Criação das rotas para Bolos/Donuts/Doces

---
+++ src/pages/Bolos/Bolos.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Bolos() {
+    return (
+        <div>
+            <h1>Bolos</h1>
+        </div>
+    );
+}
+
+export default Bolos;
---
+++ src/pages/Doces/Doces.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Doces() {
+    return (
+        <div>
+            <h1>Doces</h1>
+        </div>
+    );
+}
+
+export default Doces;
