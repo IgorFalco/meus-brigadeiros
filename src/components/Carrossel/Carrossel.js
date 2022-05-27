2aa4fcf3c16c3c9fbc1f40a0f23273313d526bfa
Igor Falco <igorfalco@cpe.com.br>
2022-05-27 16:06:01 -0300

    CSS

--- src/components/Carrossel/Carrossel.js
+++ src/components/Carrossel/Carrossel.js
@@ -7,37 +7,39 @@ import { Carousel } from 'react-bootstrap';
 function Carrossel() {
   return (
 
-    <Carousel>
-  
-      <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="/images/Imagem6.png"
-              alt="First slide"
-            ></img>
-            <Carousel.Caption>
-            </Carousel.Caption>
-          </Carousel.Item>
+      <Carousel>
 
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="/images/Imagem-8.jpg"
-              alt="Second slide" />
-            <Carousel.Caption>
-            </Carousel.Caption>
-          </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/images/Imagem6.png"
+            alt="First slide"
+          ></img>
+          <Carousel.Caption>
+          </Carousel.Caption>
+        </Carousel.Item>
 
-          <Carousel.Item> 
-            <img
-              className="d-block w-100"
-              src="/images/Imagem99.png"
-              alt="Third slide" />    
-        <Carousel.Caption>
-        </Carousel.Caption>
-      </Carousel.Item>
-    
-    </Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/images/Imagem-8.jpg"
+            alt="Second slide" />
+          <Carousel.Caption>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/images/Imagem99.png"
+            alt="Third slide" />
+          <Carousel.Caption>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+      </Carousel>
+
+
   )
 }
 
